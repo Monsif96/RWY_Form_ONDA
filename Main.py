a84b8bfd1ec1fe72daeb72c625ab22fb26b6da1b
Monsif96 <monsif.ghazaui.tab@gmail.com>
2021-05-25 16:52:30 +0100

    form v1

--- Main.py
+++ Main.py
@@ -8,6 +8,8 @@ eel.init("HTMLS")
 @eel.expose
 def Processing(Id, Idpiste, time, lrc_1, RWYCC1, RWYCC2, RWYCC3, pRWYCC1, pRWYCC2, pRWYCC3,Desc1 ,Desc2, Desc3, epaisseur1, epaisseur2, epaisseur3, largeur1, longueur, lrc_2, neige, sable, lrc_3, chimie, lrc_4, RWYcongere, Dis1, lrc_5, lrc_6, TWYcongere, Dis2, lrc_7, RWYcongere2, lrc_8, TWY, TWYstate, AIREstate, Extra):
     
+    #Section 1
+
     file = open("Section_1.txt","a")
 
     file.write(Id)
@@ -19,18 +21,21 @@ def Processing(Id, Idpiste, time, lrc_1, RWYCC1, RWYCC2, RWYCC3, pRWYCC1, pRWYCC
     file.write(Idpiste)
     file.write(lrc_1)
     file.write(" ")
+
     file.write(RWYCC1)
     file.write("/")
     file.write(RWYCC2)
     file.write("/")
     file.write(RWYCC3)
     file.write(" ")
+
     file.write(pRWYCC1)
     file.write("/")
     file.write(pRWYCC2)
     file.write("/")
     file.write(pRWYCC3)
     file.write(" ")
+
     file.write(Desc1)
     file.write("/")
     file.write(Desc2)
@@ -39,51 +44,72 @@ def Processing(Id, Idpiste, time, lrc_1, RWYCC1, RWYCC2, RWYCC3, pRWYCC1, pRWYCC
     file.write(" ")
 
     file.write(epaisseur1)
+    file.write("/")
     file.write(epaisseur2)
+    file.write("/")
     file.write(epaisseur3)
-
+    file.write("/")
     file.write(largeur1)
-
     file.write('\n') 
+    file.close()    
     
+    
+    #Section 2
+    file = open("Section_2.txt","a")
+
     file.write("RWY ") 
-    file.write(longueur)
+    file.write(Idpiste)
     file.write(lrc_2)
+    file.write(" LDA RÉDUITE À ") 
+    file.write(longueur)
     file.write(". ")
 
     file.write(neige)
-    # file.write(". ")
+    file.write(". ")
     
     file.write("RWY ")
     file.write(sable)
     file.write(lrc_3)
-    file.write(". ")
+    file.write(" SABLE NON ADHÉRENT. ")
     
     file.write("RWY ")
     file.write(chimie)
     file.write(lrc_4)
-    file.write(". ")
+    file.write(" TRAITÉE CHIMIQUEMENT. ")
 
+    file.write("RWY ")
     file.write(RWYcongere)
-    file.write(Dis1)
     file.write(lrc_5)
+    file.write(" CONGÈRE ")
+    file.write(Dis1)
     file.write(lrc_6)
+    file.write(" FM CL. ")
 
+    file.write("TWY ")
     file.write(TWYcongere)
-    file.write(Dis2)
+    file.write(" CONGÈRE ")
     file.write(lrc_7)
+    file.write(Dis2)
+    file.write(" FM CL. ")
 
+    file.write("RWY ")
     file.write(RWYcongere2)
     file.write(lrc_8)
+    file.write(" CONGÈRES ADJACENTS. ")
 
+    file.write("TWY ")
     file.write(TWY)
+    file.write(" ")
     file.write(TWYstate)
+    file.write(". ")
 
+    file.write("AIRE DE TRAFIC ")
     file.write(AIREstate)
+    file.write("MÉDIOCRE. ")
 
     file.write(Extra)
 
-    file.write('\n') 
+    file.write('.\n \n') 
     file.close()    
     
     
