40736ffcdca7a2cd996a63144157c7e661441ef8
Monsif96 <monsif.ghazaui.tab@gmail.com>
2021-05-30 13:42:42 +0100

    v2

--- HTMLS/Js/Main.js
+++ HTMLS/Js/Main.js
@@ -75,6 +75,120 @@ function Pickup() {
 
     console.log(Id, Idpiste, lrc_1, time, RWYCC1, pRWYCC1, Desc3)
     eel.Processing(Id, Idpiste, time, lrc_1, RWYCC1, RWYCC2, RWYCC3, pRWYCC1, pRWYCC2, pRWYCC3,Desc1 ,Desc2, Desc3, epaisseur1, epaisseur2, epaisseur3, largeur1, longueur, lrc_2, neige, sable, lrc_3, chimie, lrc_4, RWYcongere, Dis1, lrc_5, lrc_6, TWYcongere, Dis2, lrc_7, RWYcongere2, lrc_8, TWY, TWYstate, AIREstate, Extra )(Result)
+
+// <!-- Textarea............................................................................. -->
+
+    // <!-- First Section ............................................................................. -->
+        msg= Id +" "+ Idpiste+lrc_1+" "+RWYCC1;
+
+
+        if (Array.from(RWYCC2).length >= 1 ) {
+            msg=msg+"/"+RWYCC2;
+        }
+
+        if (Array.from(RWYCC3).length >= 1 ) {
+            msg=msg+"/"+RWYCC3+" ";
+        }
+
+        if (Array.from(pRWYCC1).length >= 1 ) {
+            msg=msg+pRWYCC1;
+        }
+
+        if (Array.from(pRWYCC2).length >= 1 ) {
+            msg=msg+"/"+pRWYCC2;
+        }
+
+        if (Array.from(pRWYCC3).length >= 1 ) {
+            msg=msg+"/"+pRWYCC3+ " ";
+        }
+
+        if (Array.from(epaisseur1).length >= 1 ) {
+            msg=msg+epaisseur1;
+        }
+
+        if (Array.from(epaisseur2).length >= 1 ) {
+            msg=msg+"/"+epaisseur2;
+        }
+
+        if (Array.from(epaisseur3).length >= 1 ) {
+            msg=msg+"/"+epaisseur3+ " ";
+        }
+
+        if (Array.from(Desc1).length >= 1 ) {
+            msg=msg+" "+Desc1;
+        }
+
+        if (Array.from(Desc2).length >= 1 ) {
+            msg=msg+"/"+Desc2;
+        }
+
+        if (Array.from(Desc3).length >= 1 ) {
+            msg=msg+"/"+Desc3+" ";
+        }
+
+         if (Array.from(largeur1).length >= 1 ) {
+            msg=msg+largeur1;
+        }
+    
+    // <!-- Section Two ............................................................................. -->
+
+        if (Array.from(longueur).length >= 1 ) {
+            msg=msg+" \n RWY "+Idpiste+lrc_2+" LDA RÉDUITE À "+longueur+". ";
+        }
+
+        if (Array.from(neige).length >= 1 ) {
+            msg=msg+neige+". ";
+        }
+
+        if (Array.from(sable).length >= 1 ) {
+            msg=msg+"RWY "+sable+lrc_3+" SABLE NON ADHÉRENT. ";
+        }
+
+        if (Array.from(chimie).length >= 1 ) {
+            msg=msg+"RWY "+chimie+lrc_4+" TRAITÉE CHIMIQUEMENT. ";
+        }
+
+        if (Array.from(Dis1).length >= 1 ) {
+            msg=msg+"RWY "+RWYcongere+lrc_5+" CONGÈRE "+Dis1+lrc_6+" FM CL. ";
+        }
+
+        if (Array.from(Dis2).length >= 1 ) {
+            msg=msg+"TWY "+TWYcongere+" CONGÈRE "+lrc_7+Dis2+" FM CL. ";
+        }
+
+        if (Array.from(RWYcongere2).length >= 1 ) {
+            msg=msg+"RWY "+RWYcongere2+lrc_8+" CONGÈRES ADJACENTS. ";
+        }
+
+        if (Array.from(TWYstate).length >= 1 ) {
+            msg=msg+"TWY "+TWY+" "+TWYstate+". ";
+        }
+
+        if (Array.from(AIREstate).length >= 1 ) {
+            msg=msg+"AIRE DE TRAFIC "+AIREstate+" MÉDIOCRE. ";
+        }
+
+        if (Array.from(Extra).length >= 1 ) {
+            msg=msg+Extra+".\n ";
+        }
+
+    document.getElementById("result").innerHTML = msg;
+
+
+    // +pRWYCC1 +"/"+pRWYCC2+"/"+pRWYCC3+" "+epaisseur1+"/"+epaisseur2+"/"+epaisseur3+" "+Desc1+"/"+Desc2+"/"+Desc3+" "+largeur1+'\n'
+    // // Section2
+    // +"RWY "+Idpiste+lrc_2+" LDA RÉDUITE À "+longueur+". "
+    // +neige+". "
+    // +"RWY "+sable+lrc_3+" SABLE NON ADHÉRENT. "
+    // +"RWY "+chimie+lrc_4+" TRAITÉE CHIMIQUEMENT. "
+    // +"RWY "+RWYcongere+lrc_5+" CONGÈRE "+Dis1+lrc_6+" FM CL. "
+    // +"TWY "+TWYcongere+" CONGÈRE "+lrc_7+Dis2+" FM CL. "
+    // +"RWY "+RWYcongere2+lrc_8+" CONGÈRES ADJACENTS. "
+    // +"TWY "+TWY+" "+TWYstate+". "
+    // +"AIRE DE TRAFIC "+AIREstate+"MÉDIOCRE. "
+        ;
+
+
 }
 
 function Result (x) {
@@ -82,3 +196,4 @@ function Result (x) {
     span.textContent = x;
     console.log(x);
 }
+
